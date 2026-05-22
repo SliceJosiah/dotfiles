72f98d326904e4c4d0e4701ea13971fc530cbd82
SliceJosiah <slicejosiah.vcuum.pm3@disroot.org>
2026-05-23 11:15:03 +1200

    Add Lua config being developed, with colour scheme

---
+++ hypr/.config/hypr/hyprland-lua-dev.lua
@@ -0,0 +1,44 @@
+-- This is Brinton's Hyprland config file :3
+-- Based off my old Hyprlang config.
+
+-- |||||||||||||||||||
+-- || Colour themes ||
+-- |||||||||||||||||||
+
+-- ||     Ninex     ||
+-- || By BrintonTua ||
+-- || for Hyprland  ||
+
+local ninex = {
+	-- || Base ||
+	Base = "#070D16",
+	Over1 = "#191C22",
+	Over2 = "#25262B",
+	Over3 = "#2C2E31",
+	Over4 = "#AEB1B5",
+	Over5 = "#CCCCCF",
+	-- || Text ||
+	MainText = "#9A9CA4",
+	DarkText = "#6B707E",
+	-- || Main colours ||
+	DarkBlue = "#1613AB",
+	MidBlue = "#0000FF",
+	LightBlue = "#5665FF",
+	DarkPurple = "#7B01A2",
+	MidPurple = "#9200CE",
+	LightPurple = "#C344FF",
+	DarkCyan = "#007C64",
+	MidCyan = "#00AF95",
+	LightCyan = "#48E6AF",
+	-- || Secondary colours ||
+	DarkOrange = "#CF7D00",
+	LightOrange = "#FF9E4E",
+	DarkGreen = "#5CCE21",
+	LightGreen = "#97F631",
+	DarkPink = "#B912AF",
+	LightPink = "#E600FE",
+	DarkRed = "#BD1B26",
+	LightRed = "#F7144C"
+},
+
+
